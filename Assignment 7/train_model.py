c130400013c394df7585cdab9f6c702cc70db9cf
PrasadYewale <prasad24024@gmail.com>
2025-07-21 00:32:24 +0530

    all files

---
+++ Assignment 7/train_model.py
@@ -0,0 +1,17 @@
+#Assignment 7 - Prasad Yewale
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
+import pandas as pd
+import joblib
+
+# Load dataset
+data = load_diabetes()
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = data.target
+
+# Train model
+model = RandomForestRegressor()
+model.fit(X, y)
+
+# Save model
+joblib.dump(model, 'diabetes_model.pkl')
